ea25678004f6cf9ce719ea34e3f19489165df8f8
Yusuf <yusf.mbk@gmail.com>
2023-03-31 15:50:34 +0100

    added package files and also finished coding the inquirer prompts for the manager

--- index.js
+++ index.js
@@ -13,3 +13,56 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const team = []
+const idArray = []
+
+function buildTeam(){
+
+}
+
+
+const menu = () => {
+
+    function createManager(){
+        console.log("Build your team!");
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is the team manager's name?",
+                validate: answers => {
+                    if(answers!== ""){
+                        return true
+                    }
+                    return "Please enter atleast a single character."
+                }
+            },
+            {
+                type:"input",
+                name:"managerId",
+                message:"What is the manager's ID?",
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is the manager's email?",
+            },
+            {
+                type:"input",
+                name:"managerOfficeNum",
+                message:"What is the manager's office number?",
+            },
+
+        ]).then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOfficeNum)
+            team.push(manager);
+            idArray.push(answers.managaerId);
+            createTeam();
+        })
+    }
+
+    createManager();
+}
+
+
+menu();
